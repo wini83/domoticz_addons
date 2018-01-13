648fac6933c1027f561a1d588aab4e6ef86cb7b0
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-01-13 18:59:13 +0100

    using pyglet

--- ansage/relay.py
+++ ansage/relay.py
@@ -4,6 +4,10 @@ import time
 import os
 import signal
 import sys
+from playsound import playsound
+import pyglet
+
+
 
 
 
@@ -16,30 +20,18 @@ try:
 	time.sleep(3)
 	GPIO.setup(27,GPIO.OUT)
 	time.sleep(1)
-	os.system("aplay gong.wav")
-	os.system("./speech.sh "+text)
+	music = pyglet.resource.media('gong.wav')
+	music.play()
+	pyglet.app.run()
+
 	time.sleep(1)
 	GPIO.cleanup(27)
 	time.sleep(2)
-	
-	#GPIO.output(tab,True)
-	
-	#GPIO.output(17,True)
-	
-	#time.sleep(5)
-	#GPIO.output(27,True)
-	#time.sleep(10)
-	#GPIO.output(27,False)
-	#time.sleep(5)
-	#GPIO.output(17,False)
-	
-	#GPIO.output(17,False)
+
 	
 
-	GPIO.cleanup(tab) #wyczysc/zresetuj oba wyjscia
-	
+	GPIO.cleanup(tab)
 
-	#os.kill(os.getppid(),signal.SIGHUP) #zamyka okno
 
 	
 except KeyboardInterrupt:
