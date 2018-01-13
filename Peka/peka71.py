9f524a8adee63a2fad61df1beca8534b4c98f5d6
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-01-13 19:10:00 +0100

    init

---
+++ Peka/peka71.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2 sty 2018
+
+
+'''
+
+import json
+import urllib2
+import time
+import urllib
+
+
+def now_milliseconds():
+  return int(time.time() * 1000)
+
+lcdUrl = "http://192.168.1.200/control?cmd=lcd,"
+
+def peka_vm_get(met,p0):
+    url = "http://www.peka.poznan.pl/vm/method.vm?ts="+str(now_milliseconds())
+    headers = {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
+    payload = urllib.urlencode({'method' : met,
+                                'p0'  : p0})
+    req = urllib2.Request(url, payload, headers)
+    response = urllib2.urlopen(req)
+
+    # Parse Json
+    data = json.load(response)
+    return data
+
+# Preety print the result
+#print json.dumps(peka_vm_get('getStopPoints','{"pattern":"IPN"}'), indent=4, sort_keys=True)
+
+#print json.dumps(peka_vm_get('getBollardsByStopPoint','{"name":"IPN"}'), indent=4, sort_keys=True)
+
+ipn01 = peka_vm_get('getTimes','{"symbol":"IPNZ01"}')
+ipn02 = peka_vm_get('getTimes','{"symbol":"IPNZ02"}')
+
+l1 = ipn01["success"]["times"][0]
+l2 = ipn02["success"]["times"][0]
+#print (json.dumps(l1, indent=4, sort_keys=True, ensure_ascii=False).encode('UTF-8'))
+print('{}>{}:{}m'.format(l1["line"],l1["direction"].encode('UTF-8'),l1["minutes"]))
+print('{} > {} : {} min'.format(l2["line"],l2["direction"].encode('UTF-8'),l2["minutes"]))
+
+
---
+++ Peka/pm.py
@@ -0,0 +1,47 @@
+import json
+import urllib2
+
+# open the url and the screen name 
+# (The screen name is the screen name of the user for whom to return results for)
+#url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=python"
+
+url = "https://airapi.airly.eu/v1/mapPoint/measurements?latitude=52.39&longitude=16.89&historyHours=24"
+apikey = "9658b2b3da0d45848fb1b312daccad29"
+normPm10 = 50
+normPM25 = 25
+
+lcdRow = 4
+
+lcdUrl = "http://192.168.1.200/control?cmd=lcd,"
+request = urllib2.Request(url)
+request.add_header("apikey", apikey)
+response = urllib2.urlopen(request)
+
+
+
+data = json.load(response)
+
+# print the result
+#print (data)
+
+#print(data["currentMeasurements"])
+
+aki =  data["currentMeasurements"]["airQualityIndex"]
+pm25 =  data["currentMeasurements"]["pm25"]
+pm10 = data["currentMeasurements"]["pm10"]
+
+
+#print (aki)
+#print(pm25)
+#print(pm10Percentage)
+
+ergebnis = 'PM10:{:.0%} PM25:{:.0%}'.format(pm10/normPm10,pm25/normPM25)
+print (ergebnis)
+
+urlOut = lcdUrl+str(lcdRow)+",1,"+urllib2.quote(ergebnis)
+
+#print(urlOut)
+
+request2 = urllib2.Request(urlOut)
+response2 = urllib2.urlopen(request2)
+
