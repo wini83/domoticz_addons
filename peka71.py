fc61f8a49eb68681cb04b68b7881b8d7b400bbab
wini83 <mariusz.wincior@gmail.com>
2018-08-04 15:24:23 +0200

    jh

--- peka71.py
+++ peka71.py
@@ -80,7 +80,7 @@ part1 = get_1st_departure_xchar("IPNZ01",9)
 part2 = get_1st_departure_xchar("IPNZ02",9)
 
 text2send = '{} {}'.format(part1,part2)
-print(text2send)
+print(text2send.encode('UTF-8'))
 display = LCDBridge()
 display.send2LCD(3, 1, text2send)
 
