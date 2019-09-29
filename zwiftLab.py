4c854e213c17d7680ba52cbc185e08e6f945c0fd
wini83 <mariusz.wincior@gmail.com>
2019-09-29 20:34:09 +0200

    'cleanup

--- zwiftLab.py
+++ zwiftLab.py
@@ -15,21 +15,21 @@ def get_activities(profile):
     return profile.get_activities()  # metadata of your activities
 
 
-def pretty_print(input):
+def pretty_print(payload):
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(input)
+    pp.pprint(payload)
 
 
-def get_TotalDistance(profile):
-    return profil[u'totalDistance'] / 1000
+def get_total_distance(profile):
+    return profile[u'totalDistance'] / 1000
 
 
 def get_world(client):
     return client.get_world(1)
 
 
-def get_players_in_world(world):
-    players = world.players
+def get_players_in_world(world_obj):
+    players = world_obj.players
     return players['friendsInWorld']
 
 
