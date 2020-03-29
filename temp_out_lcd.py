78fc76cddde0d0cb5b9fed3bf6fc9032fc46a55a
wini83 <mariusz.wincior@gmail.com>
2020-03-29 14:25:39 +0200

    temp1

--- temp_out_lcd.py
+++ temp_out_lcd.py
@@ -14,20 +14,14 @@ server = dom.Server(address=config.domoticz_ip, port=config.domoticz_port)
 dev_openweather_ws = dom.Device(server, openweather)
 
 
-print(dev_openweather_ws.data)
-
 print(dev_openweather_ws.temp)
 
 print(dev_openweather_ws.barometer)
 
-#pm10_percentage = float(dev_pm10.data.strip('%')) / 100.0
 
+ergebnis = '{}C {}hPa'.format(dev_openweather_ws.temp, dev_openweather_ws.barometer)
+print(ergebnis)
 
+lcd = lcdbridge.LCDBridge()
 
-
-#ergebnis = 'PM10:{:.0%} PM25:{:.0%}'.format(pm10_percentage, pm25_percentage).center(20)
-#print(ergebnis)
-
-#lcd = lcdbridge.LCDBridge()
-
-#lcd.send2LCD(4, 1, ergebnis)
+lcd.send2LCD(LCD_ROW, 1, ergebnis)
