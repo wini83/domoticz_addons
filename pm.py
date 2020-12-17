b595c0cd334a0a6f90d29d45dd9d2e8cc9a5d3de
wini83 <mariusz.wincior@gmail.com>
2020-12-17 21:49:08 +0100

    add_syngeos

--- pm.py
+++ pm.py
@@ -5,24 +5,27 @@ import DomoticzAPI as dom
 
 from lib import lcdbridge
 
-LCD_ROW = 4
 PM10_IDX = 5940
 PM25_IDX = 5939
+
+def compose_row(pm10,pm25):
+    pm10_percentage = float(pm10.data.strip('%')) / 100.0
+    pm25_percentage = float(pm25.data.strip('%')) / 100.0
+    ergebnis = 'PM10:{:.0%} PM25:{:.0%}'.format(pm10_percentage, pm25_percentage).center(20)
+    print(ergebnis)
+    return ergebnis
 
 
 server = dom.Server(address=config.domoticz_ip, port=config.domoticz_port)
 
-dev_pm10 = dom.Device(server, PM10_IDX)
-dev_pm25 = dom.Device(server, PM25_IDX)
+dev_airly_pm10 = dom.Device(server, PM10_IDX)
+dev_airly_pm25 = dom.Device(server, PM25_IDX)
 
-pm10_percentage = float(dev_pm10.data.strip('%')) / 100.0
-pm25_percentage = float(dev_pm25.data.strip('%')) / 100.0
+dev_syngeos_pm10 = dom.Device(server, config.syngeos_pm_10_norm)
+dev_syngeos_pm25 = dom.Device(server, config.syngeos_pm_2_5_norm)
 
-
-
-ergebnis = 'PM10:{:.0%} PM25:{:.0%}'.format(pm10_percentage, pm25_percentage).center(20)
-print(ergebnis)
 
 lcd = lcdbridge.LCDBridge()
 
-lcd.send2LCD(4, 1, ergebnis)
+lcd.send2LCD(3, 1, compose_row(dev_airly_pm10,dev_airly_pm25))
+lcd.send2LCD(3, 1, compose_row(dev_syngeos_pm10,dev_syngeos_pm25))
